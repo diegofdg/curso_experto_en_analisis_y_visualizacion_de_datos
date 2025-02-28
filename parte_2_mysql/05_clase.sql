7cdba0e181836ea60a7369846052e1e81f250c45
diegofdg <diegofdg@gmail.com>
2025-02-28 08:57:33 -0300

    parte 2_select distinct

---
+++ parte_2_mysql/05_clase.sql
@@ -0,0 +1,7 @@
+USE world;
+
+SELECT CountryCode AS 'Codigo de Pais' FROM city;
+
+SELECT DISTINCT CountryCode AS 'Codigo de Pais' FROM city;
+
+SELECT DISTINCT CountryCode AS 'Codigo de Pais', Name FROM city;
