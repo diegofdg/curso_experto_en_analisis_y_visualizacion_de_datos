7a33de9e59e9e350ab327eca9a398ec379f6a443
diegofdg <diegofdg@gmail.com>
2025-02-28 10:47:26 -0300

    parte 2_operadores de comparación

---
+++ parte_2_mysql/08_clase.sql
@@ -0,0 +1,17 @@
+USE employees;
+
+SELECT 4 < 5;
+
+SELECT 4 <= 5;
+
+SELECT 7 < 2;
+
+SELECT 7 <= 2;
+
+SELECT 8 != 8;
+
+SELECT 8 = 8;
+
+SELECT * FROM salaries WHERE salary > 60000;
+
+SELECT * FROM salaries WHERE salary != 60000;
