a320313ecd8299ea33cb4c8a90dc5ec2f7ce4041
diegofdg <diegofdg@gmail.com>
2025-02-28 08:58:36 -0300

    parte 2_operadores matemáticos

---
+++ parte_2_mysql/06_clase.sql
@@ -0,0 +1,15 @@
+USE world;
+
+SELECT 3+4;
+
+SELECT 5-2;
+
+SELECT 5*4;
+
+SELECT 41/5;
+
+SELECT 41 DIV 5;
+
+SELECT mod(41,5);
+
+SELECT Name, (Population / SurfaceArea) AS 'Habitantes por Km2' FROM country;
