fbbfa09793ca8dc3fedbbe3d52f55e13ecfa4c4e
diegofdg <diegofdg@gmail.com>
2025-03-01 11:16:52 -0300

    parte 3_tipos de datos en r

---
+++ parte_3_lenguaje_r/clase01.r
@@ -0,0 +1,38 @@
+# Caracter (character)
+print("Hola")
+print(class("Hola"))
+
+# Logico (logical)
+print(TRUE)
+print(FALSE)
+print(class(TRUE))
+print(FALSE & TRUE)
+print(TRUE & TRUE)
+
+# Numerico
+print(5)
+print(class(5))
+
+# Operaciones aritmeticas
+print(4 + 1)
+print(5 - 6)
+print(2 * 6)
+print(7 / 5)
+print(4 ^ 6)
+print(9 %% 2)
+
+# Operadores de comparacion
+print(9 < 5)
+print(7 == 9)
+print(7 != 9)
+
+# Asignación de variables
+a <- 6
+print(a)
+print(a + 7)
+
+# Reasignación de variables
+a <- 9
+print(a)
+a <- "Hola"
+print(a)
