8cf7c2d469fc14072837f1088d06241f0bccf105
diegofdg <diegofdg@gmail.com>
2025-03-01 11:25:15 -0300

    parte 3_vectores

---
+++ parte_3_lenguaje_r/clase02.r
@@ -0,0 +1,24 @@
+# Vectores
+v1 <- c(1,4,6)
+print(v1)
+
+v2 <- c("a","b","c")
+print(v2)
+
+v3 <- c(TRUE,FALSE,TRUE)
+print(v3)
+
+print(is.vector(v1))
+
+print(is.vector(9))
+
+v4 <- c(9,10)
+print(v4)
+
+v5 <- c(v1,v2)
+print(v5)
+
+v6 <- c(v1,v4)
+print(v6)
+
+print(length(v6))
