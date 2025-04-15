d6e209615d9ee8d7d070b4881eb0146e0ed4f862
diegofdg <diegofdg@gmail.com>
2025-04-15 07:33:00 -0300

    parte 3_personalización de gráficos 2

--- parte_3_lenguaje_r/clase26.r
+++ parte_3_lenguaje_r/clase26.r
@@ -1,5 +1,16 @@
 x <- c(1, 2, 3, 5)
 y <- c(4, 5, 2, 3)
 
-plot(x, y, col = "red", main = "Mi primer gráfico", xlab = "Eje X", ylab = "Eje Y", pch = 16, type = "b", lty = 2, lwd = 3) # nolint
 grid(lwd = 2, col = "blue")
+par(new = TRUE)
+plot(x, y, col = "red",
+     main = "Mi primer gráfico",
+     sub = "Mi primer subtítulo",
+     xlab = "Eje X",
+     ylab = "Eje Y",
+     pch = 16,
+     type = "b",
+     lty = 2,
+     lwd = 3,
+     xlim = c(1.5, 4.5),
+     ylim = c(2.5, 6))
